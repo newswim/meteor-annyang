0731145b834f71cfccd928b6e37553388121d5da
Daniel Minshew <ofenixculpa@gmail.com>
2015-09-21 14:39:12 -0500

    add docs, add min.js

--- packages/annyang/package.js
+++ packages/annyang/package.js
@@ -1,18 +1,15 @@
 Package.describe({
   name: 'newswim:annyang',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '1.0.0',
+  summary: 'A tiny javascript SpeechRecognition library that lets your users control your site with voice commands.',
+  git: 'https://github.com/newswim/meteor-annyang',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
   api.addFiles('annyang.js');
+  api.addFiles('annyang.min.js');
   api.export('annyang', 'client');
 });
 
